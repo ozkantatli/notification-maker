cc38fa75fbcc902f80346d4046b2ca66ee074605
ozkantatli <36783485+ozkantatli@users.noreply.github.com>
2023-12-12 23:15:42 +0300

    denemeler

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const baslik = document.getElementById('inpbaslik').innerText
-const icerik = document.getElementById('inpicerik')
+const baslik = document.getElementById('baslik')
+const icerik = document.getElementById('icerik')
+
+const inpbaslik = document.getElementById('inpbaslik').textContent
+const inpicerik = document.getElementById('inpicerik').textContent
+
+const btngonder = document.getElementById('btn-gonder')
+
+btngonder.addEventListener("click",btngonder1)
+
+function btngonder1(){
+const inpbaslik = document.getElementById('inpbaslik')
+
+document.getElementById('baslik').innerText = inpbaslik.innerText;
 
 
-console.log(baslik);
-console.log(icerik);
 
+  console.log(baslik.innerHTML);
+  console.log(icerik.innerHTML);
+}
 
